7d6c045e485466a6798cf3a8109eb26d78ffc4ac
Aditya Mohite <91630035+Aditya-Mohite@users.noreply.github.com>
2024-01-09 02:54:20 +0530

    Create CodeStudio_Area of a Rectangle.cpp

---
+++ Day_01/CodeStudio_Area of a Rectangle.cpp
@@ -0,0 +1,14 @@
+// Coding Ninjas - Que on Classes and Object
+
+#include <bits/stdc++.h> 
+class Rectangle 
+{
+    public:
+    int length;
+    int breadth;
+    int getArea()
+    {
+        return length * breadth;
+    }
+};
+
