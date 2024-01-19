ede8fbc0eb7672b90d00f5bab99e4b2088f76e4f
Aditya Mohite <91630035+Aditya-Mohite@users.noreply.github.com>
2024-01-20 00:57:02 +0530

    Create CodeStudio_Longest Subarray With Sum K.cpp

---
+++ Day_14/CodeStudio_Longest Subarray With Sum K.cpp
@@ -0,0 +1,36 @@
+// longest subarray whose sum is equal to 'k'
+
+#include <bits/stdc++.h> 
+int getLongestSubarray(vector<int>& nums, int k)
+{
+    int n = nums.size(); // size of the array.
+
+    map<long long, int> preSumMap;
+    long long sum = 0;
+    int maxLen = 0;
+    for (int i = 0; i < n; i++) {
+        //calculate the prefix sum till index i:
+        sum += nums[i];
+
+        // if the sum = k, update the maxLen:
+        if (sum == k) {
+            maxLen = max(maxLen, i + 1);
+        }
+
+        // calculate the sum of remaining part i.e. x-k:
+        long long rem = sum - k;
+
+        //Calculate the length and update maxLen:
+        if (preSumMap.find(rem) != preSumMap.end()) {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+
+        //Finally, update the map checking the conditions:
+        if (preSumMap.find(sum) == preSumMap.end()) {
+            preSumMap[sum] = i;
+        }
+    }
+
+    return maxLen;
+}
