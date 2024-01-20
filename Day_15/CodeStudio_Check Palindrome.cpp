c32a8fdc844f91e47ef88d0c13a4b4f9fbcfae06
Aditya Mohite <91630035+Aditya-Mohite@users.noreply.github.com>
2024-01-21 00:37:15 +0530

    Create CodeStudio_Check Palindrome.cpp

---
+++ Day_15/CodeStudio_Check Palindrome.cpp
@@ -0,0 +1,54 @@
+bool isValid(char ch)
+{
+    if(ch>='A' && ch<='Z' || ch>='a' && ch<='z' || ch>='0' && ch<='9')
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+char toLower(char ch)
+{
+    if(ch>='A' && ch<='Z') 
+    {
+        ch = ch - 'A' + 'a';
+        return ch;
+    }
+    else 
+    {
+        return ch;
+    }
+}
+
+bool isPalindrome(string &s)
+{
+	string temp = "";
+
+    for(int i=0; i<s.size(); i++) 
+    {
+        if(isValid(s[i])) 
+        {
+            temp.push_back(s[i]);
+        }
+    }
+
+    int i = 0;
+    int j = temp.size()-1;
+
+    while(i<j) 
+    {
+        if(toLower(temp[i])==toLower(temp[j])) 
+        {
+            i++;
+            j--;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+    return true;
+}
